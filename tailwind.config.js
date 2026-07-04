3b81de01c0d39825e1e2de58201aa79ec6728ae8
farzansa <90024136+farzansa@users.noreply.github.com>
2026-07-04 11:09:48 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // این یعنی تمام فایل‌های HTML در پوشه اصلی
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
